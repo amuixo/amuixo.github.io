015865b5d727441e00b987b8640280faef114485
Angela <angela.c.davids@gmail.com>
2025-09-27 03:09:13 +0200

    disabled the placeholders

--- idk/obj/Debug/net8.0/portfolio.AssemblyInfo.cs
+++ idk/obj/Debug/net8.0/portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b4bd388d8e8cbbeadda9f165f1e34df44716fc7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1fa317b0d91d3f66a873ca1d4e54155e50f810d")]
 [assembly: System.Reflection.AssemblyProductAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
